8f29690a2a1ad1240dacf9a0ce4ed5b55c8bd0dc
Valentyn Vasylevskyy <vasylevskyyv@ryanair.com>
2018-05-04 17:37:29 +0300

    added few more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatCardModule, MatButtonModule,
+  MatSidenavModule, MatIconModule, MatListModule, MatSidenav } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
 import { ImgCardComponent } from './img-card/img-card.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { LayoutModule } from '@angular/cdk/layout';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgCardComponent
+    ImgCardComponent,
+    AppNavComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
+    LayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
